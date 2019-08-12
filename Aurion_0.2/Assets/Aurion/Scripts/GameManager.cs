280b15a048831f1600bae475e162b3f6ab107ff9
Melissa Motoki <melissamotoki@gmail.com>
2019-08-12 17:38:18 -0300

    Script Att
    Scriptable Object: Perguntas

---
+++ Aurion_0.2/Assets/Aurion/Scripts/GameManager.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    Pergunta[] _perguntas = null;
+    public Pergunta[] Perguntas { get { return _perguntas; } }
+
+}
