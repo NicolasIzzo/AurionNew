46c394f38dfd4d4795b3f9b6e7cd9974c3987a8b
Renan <renan.barbieri.s@gmail.com>
2019-08-12 16:56:18 -0300

    Arkanoid_desk_pronto

---
+++ Arkanoid/Arkanoid/Arkanoid/Assets/Scripts_Materiais/Destroyer_ice.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Destroyer_ice : MonoBehaviour
+{
+    
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        GameObject collider = collision.collider.gameObject;
+        GameObject.Destroy(collider);
+        Debug.Log("BOLINHA FOI PRO BREJO");
+        Debug.Log("Quit");
+        Debug.Log("PONTUAÇÃO:" + Bloco.pontos);
+        //Show_point.MostraPontos(Bloco.pontos);
+        // Application.Quit();
+        
+    }
+ 
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+        GameObject.Destroy(collider.gameObject);
+    }
+}
