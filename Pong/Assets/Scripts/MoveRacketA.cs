0fea690321c644cc5999a9eac5f50e92709a7aff
barbieri <asdasd>
2019-08-16 11:49:17 -0300

    Pong pronto
    pong ta pronto mlkada

---
+++ Pong/Assets/Scripts/MoveRacketA.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveRacketA : MonoBehaviour
+{
+    public float speed;
+    public string axis;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void FixedUpdate()
+    {
+        float v = Input.GetAxisRaw(axis);
+        GetComponent<Rigidbody2D>().velocity = new Vector2(0, v) * speed;
+    }
+}
