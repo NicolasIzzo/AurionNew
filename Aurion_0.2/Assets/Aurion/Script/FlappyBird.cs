9bdb98ecaf6970c62a394f48c54b87d931476c08
Melissa Motoki <melissamotoki@gmail.com>
2019-08-25 16:19:41 -0300

    Ajustes finos
    Ajustes de Interface!

---
+++ Aurion_0.2/Assets/Aurion/Script/FlappyBird.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class FlappyBird : MonoBehaviour
+{
+    /*public Button MyButton;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Button btn = MyButton.GetComponent<Button>();
+        MyButton.onClick.AddListener(TaskOnClick);
+    }*/
+
+    public void TaskOnClick()
+    {
+        SceneManager.LoadScene("FlappyBird");
+    }
+}
