2d093d96ee94e4407779a09a7018193f0218e2e5
Melissa Motoki <melissamotoki@gmail.com>
2019-08-27 18:18:19 -0300

    SQLite funcionando Android
    Não sei o que eu fiz, mas agora é pra rodar em Android. (e roda, no caso)

---
+++ Aurion_0.2/Assets/Aurion/Scripts/PlaceholderControl.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class PlaceholderControl : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public TMPro.TMP_InputField txt;
+    void Start()
+    {
+        TMPro.TMP_InputField input = txt.GetComponent<TMPro.TMP_InputField>();
+        txt.onSelect.AddListener(TaskOnSelect);
+    }
+
+    public void TaskOnSelect(string ae)
+    {
+          txt.placeholder.GetComponent<TMPro.TMP_Text>().text = "";
+          txt.text ="";
+    }
+}
